4c5651ef93595e821dea94eda1a1ff94e6b218ec
Admin <ptnamit.work@gmail.com>
2023-04-08 14:42:50 +0700

    Update

--- RPA HUTECH/Asset/Update git.bat
+++ RPA HUTECH/Asset/Update git.bat
@@ -1,8 +1,13 @@
 @echo off
 setlocal
-set "WORKING_DIR=E:\RPA\"
+taskkill /f /im UiPath.Executor.exe
+set "WORKING_DIR=D:\RPA\"
 cd /d "%WORKING_DIR%"
 git pull origin master
 cd /d "%~dp0"
 timeout /t 3
+
+echo Starting bot...
+"C:\Users\Administrator\AppData\Local\Programs\UiPath\Studio\UiRobot.exe" -file "D:\RPA\RPA HUTECH\Metabot.xaml"
 endlocal
+
